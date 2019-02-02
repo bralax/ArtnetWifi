ce11a17de78bc4279ed3928a7e566bc80de84c39
bralax <35619281+bralax@users.noreply.github.com>
2019-02-01 23:51:23 -0500

    Update ArtnetWifiTransmit.ino

--- examples/ArtnetWifiTransmit/ArtnetWifiTransmit.ino
+++ examples/ArtnetWifiTransmit/ArtnetWifiTransmit.ino
@@ -3,12 +3,7 @@ This example will transmit a universe via Art-Net into the Network.
 This example may be copied under the terms of the MIT license, see the LICENSE file for details
 */
 
-#if defined(ARDUINO_ARCH_ESP32)
-#include <WiFi.h>
-#else
-#include <ESP8266WiFi.h>
-#endif
-#include <WiFiUdp.h>
+#include <WifiNINA.h>
 #include <ArtnetWifi.h>
 
 //Wifi settings
